cb92f63e12ce9bedfaecb74d1523be4c3bd91c77
Erin Linley <elinley1@gmail.com>
2018-05-23 15:59:57 -0400

    Added restify for the mongoose models.

--- server/server.js
+++ server/server.js
@@ -1,35 +1,40 @@
 // Loading evnironmental variables here
 if (process.env.NODE_ENV !== 'production') {
-	console.log('loading dev environments')
-	require('dotenv').config()
+    console.log('loading dev environments')
+    require('dotenv').config()
 }
 require('dotenv').config()
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const dbConnection = require('./db') // loads our connection to the mongo database
-const passport = require('./passport')
-const app = express()
-const PORT = process.env.PORT || 8080
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const dbConnection = require('./db'); // loads our connection to the mongo database;
+const passport = require('./passport');
+const app = express();
+const restify = require('express-restify-mongoose');
+
+const PORT = process.env.PORT || 8080;
+const router = express.Router();
+const db = require('./db/models');
+
 
 // ===== Middleware ====
 app.use(morgan('dev'))
 app.use(
-	bodyParser.urlencoded({
-		extended: false
-	})
+    bodyParser.urlencoded({
+        extended: false
+    })
 )
 app.use(bodyParser.json())
 app.use(
-	session({
-		secret: process.env.APP_SECRET || 'this is the default passphrase',
-		store: new MongoStore({ mongooseConnection: dbConnection }),
-		resave: false,
-		saveUninitialized: false
-	})
+    session({
+        secret: process.env.APP_SECRET || 'this is the default passphrase',
+        store: new MongoStore({ mongooseConnection: dbConnection }),
+        resave: false,
+        saveUninitialized: false
+    })
 )
 
 // ===== Passport ====
@@ -60,28 +65,32 @@ app.use(passport.session()) // will call the deserializeUser
 
 // ==== if its production environment!
 if (process.env.NODE_ENV === 'production') {
-	const path = require('path')
-	console.log('YOU ARE IN THE PRODUCTION ENV')
-	app.use('/static', express.static(path.join(__dirname, '../build/static')))
-	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, '../build/'))
-	})
+    const path = require('path')
+    console.log('YOU ARE IN THE PRODUCTION ENV')
+    app.use('/static', express.static(path.join(__dirname, '../build/static')))
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../build/'))
+    })
 }
 
 /* Express app ROUTING */
 app.use('/auth', require('./routes/auth'))
 app.use('/api', require('./routes/api'))
+restify.serve(router, db.User);
+restify.serve(router, db.Business);
+restify.serve(router, db.Blog);
+app.use(router);
 
 // ====== Error handler ====
 app.use(function(err, req, res, next) {
-	console.log('====== ERROR =======')
-	console.error(err.stack)
-	res.status(500)
+    console.log('====== ERROR =======')
+    console.error(err.stack)
+    res.status(500)
 })
 
 // ==== Starting Server =====
 app.listen(PORT, () => {
-	console.log(`App listening on PORT: ${PORT}`)
+    console.log(`App listening on PORT: ${PORT}`)
 });
 
 module.exports = app;
