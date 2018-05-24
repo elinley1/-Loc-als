b0086a42fb27a8145bf7ba8fcd555f5d78b05fde
Erin Linley <elinley1@gmail.com>
2018-05-24 12:27:48 -0400

    Adding div for blog posts.

--- src/Pages/userProfile/userProfile.js
+++ src/Pages/userProfile/userProfile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {FormControl, FormGroup, ControlLabel, HelpBlock, Checkbox, Radio, Button} from 'react-bootstrap';
+import { FormControl, FormGroup, ControlLabel, HelpBlock, Checkbox, Radio, Button } from 'react-bootstrap';
 import axios from 'axios'
 import { Redirect } from 'react-router-dom'
 import { Col, Row, Grid } from "react-bootstrap";
@@ -7,7 +7,7 @@ import { Col, Row, Grid } from "react-bootstrap";
 class UserProfile extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ""};
+    this.state = { value: "" };
     console.log("user profile props", props);
 
     this.handleChange = this.handleChange.bind(this);
@@ -16,7 +16,7 @@ class UserProfile extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
   handleSubmit(event) {
@@ -25,32 +25,56 @@ class UserProfile extends React.Component {
   }
 
   getUsername() {
-      return this.props.user ? this.props.user.local.username : "";
+    return this.props.user ? this.props.user.local.username : "";
   }
 
 
   getEmail() {
-      return this.props.user ? this.props.user.email : "";
+    return this.props.user ? this.props.user.email : "";
   }
 
   getFirstname() {
-      return this.props.user ? this.props.user.firstName : "";
+    return this.props.user ? this.props.user.firstName : "";
   }
 
   getLastname() {
-      return this.props.user ? this.props.user.lastName : "";
+    return this.props.user ? this.props.user.lastName : "";
   }
 
-  render() {
-    return (
-        <div>
-            <h1>Username: {this.getUsername()}</h1> 
-            <h1>Email: {this.getEmail()}</h1> 
-            <h1>Firstname: {this.getFirstname()}</h1> 
-            <h1>Lastname: {this.getLastname()}</h1> 
-      </div>
-    );
-  }
-}
+  getBlogs() {
+    const userId = window.sessionStorage.getItem("userId");
+    return axios.post('/api/v1/Blog', {
+      title: this.state.title,
+      body: this.state.body,
+      rating: this.state.rating,
+      author: userId
+    }).then(response => {
+      console.log(response)
+      if (!response.data.errmsg) {
+        console.log('blog submited')
+        this.setState({
+          redirectTo: '/'
+        })
+      } else {
+        console.log("err")
+      }
+    })
+  };
 
+render() {
+  return (
+    <div>
+      <h1>Username: {this.getUsername()}</h1>
+      <h1>Email: {this.getEmail()}</h1>
+      <h1>Firstname: {this.getFirstname()}</h1>
+      <h1>Lastname: {this.getLastname()}</h1>
+    
+      <div>
+        <ul>Your Blog Posts</ul>
+        <li>{this.getBlogs()}</li>
+      </div> 
+    </div>   
+  );
+};
+};
 export default UserProfile;
