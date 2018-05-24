3580dab99f2baca872388dbb55b277c2528e87d3
Erin Linley <elinley1@gmail.com>
2018-05-24 19:39:49 -0400

    Added post creation.

--- src/Pages/Business/businessPage.js
+++ src/Pages/Business/businessPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { FormControl, FormGroup, ControlLabel, HelpBlock, Checkbox, Radio, Button } from 'react-bootstrap';
 import axios from 'axios'
 import { Redirect } from 'react-router-dom'
-import {Jumbotron, Panel, Label, Well, PageHeader, Col, Row, Grid } from "react-bootstrap";
+import {Input, TextArea, Jumbotron, Panel, Label, Well, PageHeader, Col, Row, Grid } from "react-bootstrap";
 
 class BusinessPage extends React.Component {
   constructor(props) {
@@ -10,7 +10,8 @@ class BusinessPage extends React.Component {
     this.state = {
       value: "",
       business: {},
-      blogs: []
+      blogs: [],
+      title: "", body: "", rating: ""
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -45,12 +46,27 @@ class BusinessPage extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
+		this.setState({
+			[event.target.name]: event.target.value
+    })
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
     event.preventDefault();
+    let newMessage = {
+      title: this.state.title,
+      rating: this.state.rating,
+      body: this.state.body,
+      business: this.state.business._id,
+      author: this.props.user._id
+    }
+    console.log("New Message obj", newMessage)
+    axios.post("/api/v1/Blog", newMessage)
+      .then(response => {
+        console.log("Create Blog Response", response);
+        this.setState({title:  "", rating: "", body: ""})
+        this.getBusiness()
+      });
   }
 
   getBusinessField(field) {
@@ -67,7 +83,50 @@ class BusinessPage extends React.Component {
           </Col>
 
           <Col md={6}>
-            <h2>Posts</h2>
+            <h2>Make A Post</h2>
+            
+            <form>
+              <FormGroup
+                controlId="formBasicText" >
+                <ControlLabel>Title</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.title}
+                  name="title"
+                  placeholder="Title"
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+              <FormGroup
+                controlId="formBasicText" >
+                <ControlLabel>Rating</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.rating}
+                  name="rating"
+                  placeholder="Rating"
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+              <FormGroup
+                controlId="formBasicText" >
+                <ControlLabel>Message</ControlLabel>
+                <FormControl
+                  componentClass="textarea"
+                  value={this.state.body}
+                  name="body"
+                  placeholder="Message"
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+
+              <Button bsStyle="success" onClick={this.handleSubmit} type="submit">Message</Button>
+
+
+            </form>
+
+            <h2>Your Posts</h2>
+
             {this.state.blogs.length ? this.state.blogs.map(blog => (
               <Well key={blog._id}>
                 <h4>{blog.title}</h4>
